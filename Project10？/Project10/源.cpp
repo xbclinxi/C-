add4f2aec8083b57aa393c88901653e6e9ca6761
xbclinxi <111215965+xbclinxi@users.noreply.github.com>
2022-10-03 12:39:59 +0800

    Add files via upload

---
+++ Project10？/Project10/源.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+extern int Add(int a, int b);
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum = Add(a, b);
+	printf("sum = %d\n", sum);
+	return 0;
+}
