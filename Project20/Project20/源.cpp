9342bcde3933cf001d8b9708620c0d8dc28113d9
xbclinxi <111215965+xbclinxi@users.noreply.github.com>
2022-10-13 08:58:37 +0800

    Add files via upload

---
+++ Project20/Project20/源.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int ret = 0;
+	char password[20] = { 0 };
+	int ch = 0;
+	printf("请输入密码:>");
+	scanf("%s", password);
+	while ((ch = getchar() != '\n'))
+	{
+		;
+	}
+	printf("请确认（Y/N）:>");
+	ret = getchar();
+	if (ret == 'Y')
+		printf("确认成功\n");
+	else
+		printf("放弃确认\n");
+	return 0;
+}
