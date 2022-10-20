5fabe751012c2ff3377a7f92f7cd2d78ff1b6b71
xbclinxi <111215965+xbclinxi@users.noreply.github.com>
2022-10-20 13:35:18 +0800

    Add files via upload

---
+++ Project31/Project31/源.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int year = 0;
+	int count = 0;
+	/*if(((year%4++0)&&(year%100!=0))||(year%400==0))
+	{
+		printf("%d ",year);
+		count++;
+	}*/
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (year % 4 == 0 && year % 100 != 0)
+		{
+			printf("%d ", year);
+			count++;
+		}
+		else if (year & 400==0)
+		{
+			printf("%d ", year);
+			count++;
+		}
+	}
+	printf("\ncount=%d\n", count);
+	return 0;
+}
