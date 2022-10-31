ed33ae8f64708b8f0ee1f4f97d85db9fd46fef40
xbclinxi <111215965+xbclinxi@users.noreply.github.com>
2022-10-31 08:14:18 +0800

    Add files via upload

---
+++ Project54/Project54/源.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int Fac1(int n)
+{
+	int i = 0;
+	int ret = 1;
+	for (i = 1; i <= n; i++)
+	{
+		ret *= i;
+	}
+	return ret;
+}
+
+int Fac2(int n)
+{
+	if (n <= 1)
+		return 1;
+	else
+		return n*Fac2(n - 1);
+}
+
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = Fac1(n);
+	printf("%d\n", ret);
+	return 0;
+}
