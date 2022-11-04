f0b545ab02294f3588e62c021a011b0705c03ccb
xbclinxi <111215965+xbclinxi@users.noreply.github.com>
2022-11-04 08:28:57 +0800

    Add files via upload

---
+++ Project61/Project61/源.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int i = 0;
+
+	for (i = 0; i < sz; i++)
+	{
+		printf("&arr[%d] = %p\n", i, &arr[i]);
+	}
+
+	return 0;
+}
