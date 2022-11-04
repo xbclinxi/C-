5a89292c6d9374e07816077c9473d290618ad0d9
xbclinxi <111215965+xbclinxi@users.noreply.github.com>
2022-11-04 08:29:41 +0800

    Add files via upload

---
+++ Project62/Project62/源.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int arr[3][4] = { { 1, 2, 3 },{4, 5} };
+	int i = 0;
+
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
