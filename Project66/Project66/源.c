1089c34be08b4af3fa20430640e51748b3632207
xbclinxi <111215965+xbclinxi@users.noreply.github.com>
2022-11-06 08:52:52 +0800

    Add files via upload

---
+++ Project66/Project66/源.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, };
+
+	printf("%p\n", arr);
+	printf("%p\n", arr + 1);
+
+	printf("%p\n", &arr[0]);
+	printf("%p\n", &arr[0] + 1);
+
+	printf("%p\n", &arr);
+	printf("%p\n", &arr + 1);
+
+	return 0;
+}
