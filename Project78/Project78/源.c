0bd371c53a8e5c68dde0af5dc910939f8e1e23c1
xbclinxi <111215965+xbclinxi@users.noreply.github.com>
2022-11-14 08:41:08 +0800

    Add files via upload

---
+++ Project78/Project78/源.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int i, j, m=0, n=0;
+	i = 8;
+	j = 10;
+	m += i++; n -= --j;
+	printf("i=%d,j=%d,m=%d,n=%d\n", i, j, m, n);
+	return 0;
+}
