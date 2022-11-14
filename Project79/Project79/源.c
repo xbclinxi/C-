0dbbada4bc10f029e8a3747db78ad76229c0efc1
xbclinxi <111215965+xbclinxi@users.noreply.github.com>
2022-11-15 07:50:33 +0800

    Add files via upload

---
+++ Project79/Project79/源.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int a, b, c, max;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a > b)
+	{
+		max = a;
+	}
+	else
+	{
+		max = b;
+	}
+	if (b > max)
+	{
+		max = b;
+	}
+	if (c > max)
+	{
+		max = c;
+	}
+	printf("max = %d", max);
+	return 0;
+}
